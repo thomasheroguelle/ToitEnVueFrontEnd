f36d856993f0638eab999e82c25c49a8c9531a39
Thomas HEROGUELLE <totosh93270@gmail.com>
2024-05-09 20:22:16 +0200

    feat/homePage done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HousingListPageComponent } from './pages/housing-list-page/housing-list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'housings', component: HousingListPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
