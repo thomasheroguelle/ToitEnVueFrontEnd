2fc705ad89d6ac7c35f56e5ae228f385d7c4d3ca
THOMAS HEROGUELLE <112635176+thomasheroguelle@users.noreply.github.com>
2024-05-12 00:05:54 +0200

    feat/getHousingList : feature works (#23)
    Co-authored-by: Thomas HEROGUELLE <totosh93270@gmail.com>

--- src/app/components/housing-list/housing-list.component.ts
+++ src/app/components/housing-list/housing-list.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IHousing } from '../../../interfaces/IHousing';
+import { Observable } from 'rxjs';
+import { HousingCRUDService } from '../../services/housing/housing-crud.service';
 
 @Component({
   selector: 'app-housing-list',
   templateUrl: './housing-list.component.html',
   styleUrl: './housing-list.component.css',
 })
-export class HousingListComponent {}
+export class HousingListComponent {
+  housesList: IHousing[] = [];
+
+  constructor(private allHousingList: HousingCRUDService) {}
+
+  ngOnInit() {
+    this.getHouses();
+  }
+
+  getHouses(): Observable<IHousing[]> {
+    this.allHousingList.getHousingList().subscribe(
+      (response) => {
+        console.log(response, 'housings');
+        this.housesList = response;
+      },
+      (error) => {
+        console.error(error, 'erreur');
+      },
+    );
+    return this.allHousingList.getHousingList();
+  }
+}
