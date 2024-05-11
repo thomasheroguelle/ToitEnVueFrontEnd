fa15d0b91f3f08db98ea1b20506c262bed6d1895
THOMAS HEROGUELLE <112635176+thomasheroguelle@users.noreply.github.com>
2024-05-11 14:59:02 +0200

    feat/logout : feature works (#14)
    Co-authored-by: Thomas HEROGUELLE <totosh93270@gmail.com>

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { StorageService } from '../../services/storage/storage.service';
+import { AuthService } from '../../services/authentication/auth.service';
+import { EventBusService } from '../../services/shared/event-bus.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.storageService.clean();
+        window.location.reload();
+        this.router.navigate(['housings']);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+}
