9620bc5ce83ffbf9aae17868fcfa1ea3bf3b2467
THOMAS HEROGUELLE <112635176+thomasheroguelle@users.noreply.github.com>
2024-05-12 11:55:12 +0200

    feat/goBackBtn : feature works (#31)
    Co-authored-by: Thomas HEROGUELLE <totosh93270@gmail.com>

--- src/app/components/go-back/go-back.component.ts
+++ src/app/components/go-back/go-back.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-go-back',
   templateUrl: './go-back.component.html',
   styleUrl: './go-back.component.css',
 })
-export class GoBackComponent {}
+export class GoBackComponent {
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['housings']);
+  }
+}
