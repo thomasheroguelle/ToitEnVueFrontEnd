926f41abbd5775ce3c49ac1c6a8a1fdcde80118b
Thomas HEROGUELLE <totosh93270@gmail.com>
2024-05-12 11:35:55 +0200

    feat/togglrViewBtn : feature works

--- src/app/components/toggle-view/toggle-view.component.ts
+++ src/app/components/toggle-view/toggle-view.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toggle-view',
   templateUrl: './toggle-view.component.html',
   styleUrl: './toggle-view.component.css',
 })
-export class ToggleViewComponent {}
+export class ToggleViewComponent {
+  constructor(private route: Router) {}
+
+  isOnProductsPage(): boolean {
+    const currentUrl = this.route.url;
+    if (currentUrl === '/housings') {
+      return true;
+    } else return false;
+  }
+  isOnMapPage(): boolean {
+    const currentUrl = this.route.url;
+    if (currentUrl === '/map') {
+      return true;
+    } else return false;
+  }
+}
